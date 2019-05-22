5a949857f9ef1d59bed9d1328e89cd855ad9185b
khizerrehan <khizerrehan92@gmail.com>
2019-05-22 22:16:32 +0500

    cra-cart-app: center align card with bootstrap classes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Subtotal from './components/Subtotal/Subtotal';
-import PickupSavings from './components/PickupSavings/PickupSavings'
+import PickupSavings from './components/PickupSavings/PickupSavings';
+import TaxesFees  from './components/TaxesFees/TaxesFees';
+import { Container, Row, Col } from 'react-bootstrap';
+
 import './App.css';
 
 class App extends Component {
@@ -8,18 +11,26 @@ class App extends Component {
    super(props);
    this.state= {
      total:100,
-     pickupSavings: -3.85
+     pickupSavings: -3.85,
+     taxes:0
    }
  }
 
   render() {
     return (
-      <div className="container">
-        <header className="purchase-card">
-         <Subtotal price={this.state.total.toFixed(2)} />
-         <PickupSavings price={this.state.pickupSavings.toFixed(2)}/>
-        </header>
-      </div>
+      <Container>
+        <Row>
+          <Col md={{ span: 4, offset: 4 }}>
+            <div className="container">
+              <header className="purchase-card">
+              <Subtotal price={this.state.total.toFixed(2)} />
+              <PickupSavings price={this.state.pickupSavings.toFixed(2)}/>
+              <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+              </header>
+            </div>
+            </Col>
+            </Row>
+      </Container>
     );
   }
 }
