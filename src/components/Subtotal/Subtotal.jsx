9185ec665d1f0b445b0e3cf08c36f5b1ea62c382
khizerrehan <khizerrehan92@gmail.com>
2019-05-23 01:59:15 +0500

    cra-cart-app: removed class

--- src/components/Subtotal/Subtotal.jsx
+++ src/components/Subtotal/Subtotal.jsx
@@ -4,7 +4,7 @@ import { Row, Col } from 'react-bootstrap';
 export default class Subtotal extends Component {
     render() {
         return (
-           <Row className="show-grid">
+           <Row>
                <Col md={6}>Subtotal</Col>
                <Col md={6}>{`$${this.props.price}`}</Col>
            </Row>
