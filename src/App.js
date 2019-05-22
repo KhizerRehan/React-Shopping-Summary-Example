dd541626c9ccb2a11cfb4c38520b83a222364798
khizerrehan <khizerrehan92@gmail.com>
2019-05-22 22:20:44 +0500

    cra-cart-app: structure layout fix and add css class.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
+
+import React, {Component} from 'react';
 import Subtotal from './components/Subtotal/Subtotal';
 import PickupSavings from './components/PickupSavings/PickupSavings';
-import TaxesFees  from './components/TaxesFees/TaxesFees';
-import { Container, Row, Col } from 'react-bootstrap';
+import TaxesFees from './components/TaxesFees/TaxesFees';
+import {Container, Row, Col} from 'react-bootstrap';
 
 import './App.css';
 
 class App extends Component {
- constructor(props) {
-   super(props);
-   this.state= {
-     total:100,
-     pickupSavings: -3.85,
-     taxes:0
-   }
- }
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: 100,
+            pickupSavings: -3.85,
+            taxes: 0
+        }
+    }
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col md={{ span: 4, offset: 4 }}>
-            <div className="container">
-              <header className="purchase-card">
-              <Subtotal price={this.state.total.toFixed(2)} />
-              <PickupSavings price={this.state.pickupSavings.toFixed(2)}/>
-              <TaxesFees taxes={this.state.taxes.toFixed(2)} />
-              </header>
-            </div>
-            </Col>
-            </Row>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container className="container">
+                <Row>
+                    <Col className="purchase-card" md={{span: 6, offset: 4}}>
+                        <Subtotal price={this.state.total.toFixed(2)}/>
+                        <PickupSavings price={this.state.pickupSavings.toFixed(2)}/>
+                        <TaxesFees taxes={this.state.taxes.toFixed(2)}/>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default App;
