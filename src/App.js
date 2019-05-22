6c1d2a2629eeafdeb851a3149b8670e41329ced8
khizerrehan <khizerrehan92@gmail.com>
2019-05-22 22:05:11 +0500

    cra-cart-app: used Picksaving component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Subtotal from './components/Subtotal/Subtotal';
+import PickupSavings from './components/PickupSavings/PickupSavings'
 import './App.css';
 
 class App extends Component {
  constructor(props) {
    super(props);
    this.state= {
-     total:100
+     total:100,
+     pickupSavings: -3.85
    }
  }
 
@@ -15,6 +17,7 @@ class App extends Component {
       <div className="container">
         <header className="purchase-card">
          <Subtotal price={this.state.total.toFixed(2)} />
+         <PickupSavings price={this.state.pickupSavings.toFixed(2)}/>
         </header>
       </div>
     );
