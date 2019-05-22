1b07b4787564e9c78e862ba5b70224374fd2859b
khizerrehan <khizerrehan92@gmail.com>
2019-05-22 18:08:44 +0500

    cra-cart-app: updated functional comp `App` to ES6 class base comp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import { Grid } from 'react-bootstrap';
+import React, { Component } from 'react';
 import './App.css';
 
+class App extends Component {
+ constructor(props) {
+   super(props);
+   this.state= {
+     total:100
+   }
+ }
 
-function App() {
-  return (
-    <div className="container">
-      <header className="purchase-card">
-       <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="container">
+        <header className="purchase-card">
+         <h1>Hello World</h1>
+        </header>
+      </div>
+    );
+  }
 }
-export default App
+
+export default App;
